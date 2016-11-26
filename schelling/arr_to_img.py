94a90021a78383fd7190e9d7707194a907bc1328
Stas Horawa <stashorawa@gmail.com>
2016-11-26 22:22:41 +0100

    Adding array to image conversion

---
+++ schelling/arr_to_img.py
@@ -0,0 +1,61 @@
+from numpy import vectorize, dstack, unique, amax
+from matplotlib.image import imsave
+from skimage.transform import resize
+
+
+def _get_color(color_index):
+	try:
+		return _get_color.colors[color_index]
+	except AttributeError:
+		w = (255, 255, 255)
+		r = (255, 0, 0)
+		g = (0, 255, 0)
+		b = (0, 0, 255)
+		c = (0, 255, 255)
+		m = (255, 0, 255)
+		y = (255, 255, 0)
+		k = (0, 0, 0)
+
+		_get_color.colors = (w, r, g, b, c, m, y, k)
+		return _get_color.colors[color_index]
+
+
+def to_image(array, size=800):
+	"""Convert array to rgb image.
+	White represents vacancy
+	RGBCMYK represent agents of up to 7 types.
+
+	Args:
+	    array (ndarray): 2d square array
+	    size (int, optional): size in pixels of output image (default 800)
+	
+	Returns:
+	    ndarray: array representing image
+	"""
+	if unique(array).size > 8:
+		raise ValueError("Array contains more than 8 unique elements. Only 8 colors are currently supported")
+
+	if amax(array) > 7:
+		raise ValueError("Array contains value >7.")
+
+	# get tuple of 3 arrays with each rgb component
+	get_rgb_arrays = vectorize(_get_color)
+	rgb_arrays = get_rgb_arrays(array)
+
+	# zip the arrays along new third axis
+	image_array = dstack(rgb_arrays).astype('uint8')
+
+	image_array = resize(image_array, (size, size), order=0, preserve_range=True).astype('uint8')
+
+	return image_array
+
+
+def image_save(image_array, output):
+	"""Save image to file
+	
+	Args:
+	    image_array (ndarray): 3d array representing rgb image
+	    output (file): file to save
+	"""
+	# TODO convert this to use either scipy or skimage, so matplotlib requirement can be removed.
+	imsave(output, image_array)
---
+++ schelling/test/test_arr_to_img.py
@@ -0,0 +1,50 @@
+import unittest
+from numpy import array, array_equal
+from ..arr_to_img import to_image
+
+class ArrToImgTest(unittest.TestCase):
+
+	def test_expected_output(self):
+		c0 = [255, 255, 255]
+		c1 = [255, 0, 0]
+		c6 = [255, 255, 0]
+
+
+		c = [
+			[255, 255, 255],
+			[255, 0, 0],
+			[0, 255, 0],
+			[0, 0, 255],
+			[0, 255, 255],
+			[255, 0, 255],
+			[255, 255, 0],
+			[0, 0, 0],
+		]
+		parameters = [
+			(array([[0, 1], [0, 1]]), array([[c[0], c[1]], [c[0], c[1]]])),
+			(array([[0, 1, 6], [0, 1, 6], [0, 1, 6]]), array([[c[0], c[1], c[6]], [c[0], c[1], c[6]], [c[0], c[1], c[6]]])),
+			(
+				array(list(range(8))*8).reshape(8,8),
+				array([c[x] for x in range(8)] * 8).reshape(8,8,3),
+			)
+		]
+
+		for arr_in, expected_arr_out in parameters:
+			with self.subTest(arr_in=arr_in):
+				arr_out = to_image(arr_in, size=arr_in.shape[0])
+				self.assertTrue(array_equal(arr_out, expected_arr_out))
+
+
+	def test_eight_colors_supported_error(self):
+		arr = array(list(range(10))*10).reshape((10, 10))
+		with self.assertRaises(ValueError):
+			to_image(arr)
+
+
+	def test_value_over_seven_error(self):
+		arr = array([[0, 10], [0, 10]])
+		with self.assertRaises(ValueError):
+			to_image(arr)
+
+if __name__ == '__main__':
+	unittest.main()
