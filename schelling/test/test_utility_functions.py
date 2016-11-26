60c6b490acfe1232038f360b8ba2278cc078c80d
Stas Horawa <stashorawa@gmail.com>
2016-11-26 22:04:19 +0100

    Adding utility functions

---
+++ schelling/test/test_utility_functions.py
@@ -0,0 +1,123 @@
+import unittest
+from ..utility_functions import create_flat_utility, create_peaked_utility, create_spiked_utility 
+from numpy import linspace
+
+class UtilityFunctionsTest(unittest.TestCase):
+
+	def test_flat_utility(self):
+		# step threshold, exepcted output for 0.0 - 1.0, step 0.1
+		test_data = [
+			(0.0 , [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+			(0.25, [1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+			(0.5 , [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+			(0.75, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0]),
+			(1.0 , [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]),	
+		]
+
+
+		for threshold, exepcted_output_list in test_data:
+			flat_utility = create_flat_utility(threshold)
+			self.check_expected_output_for_range_0_1('flat_'+str(threshold), flat_utility, exepcted_output_list)
+
+
+
+	def test_peaked_utility(self):
+		# peak, exepcted output for 0.0 - 1.0, step 0.1
+		test_data = [
+			(0.00, [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]),
+			(0.10, [0, 1, 0.888888889, 0.777777778, 0.666666667, 0.555555556, 0.444444444, 0.333333333, 0.222222222, 0.111111111, 0]),
+			(0.20, [0, 0.5, 1, 0.875, 0.75, 0.625, 0.5, 0.375, 0.25, 0.125, 0]),
+			(0.30, [0, 0.333333333, 0.666666667, 1, 0.857142857, 0.714285714, 0.571428571, 0.428571429, 0.285714286, 0.142857143, 0]),
+			(0.40, [0, 0.25, 0.5, 0.75, 1, 0.833333333, 0.666666667, 0.5, 0.333333333, 0.166666667, 0]),
+			(0.50, [0, 0.2, 0.4, 0.6, 0.8, 1, 0.8, 0.6, 0.4, 0.2, 0]),
+			(0.60, [0, 0.166666667, 0.333333333, 0.5, 0.666666667, 0.833333333, 1, 0.75, 0.5, 0.25, 0]),
+			(0.70, [0, 0.142857143, 0.285714286, 0.428571429, 0.571428571, 0.714285714, 0.857142857, 1, 0.666666667, 0.333333333, 0]),
+			(0.80, [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1, 0.5, 0]),
+			(0.90, [0, 0.111111111, 0.222222222, 0.333333333, 0.444444444, 0.555555556, 0.666666667, 0.777777778, 0.888888889, 1, 0]),
+			(1.00, [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]),
+		]
+
+		for peak, exepcted_output_list in test_data:
+			peaked_utility = create_peaked_utility(peak)
+			self.check_expected_output_for_range_0_1('peaked_'+str(peak), peaked_utility, exepcted_output_list)
+
+
+
+	def test_peaked_utility_cutoff(self):
+		test_data = [
+			(0.00, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+			(0.10, [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+			(0.20, [0, 0.5, 1, 0, 0, 0, 0, 0, 0, 0, 0]),
+			(0.30, [0, 0.333333333, 0.666666667, 1, 0, 0, 0, 0, 0, 0, 0]),
+			(0.40, [0, 0.25, 0.5, 0.75, 1, 0, 0, 0, 0, 0, 0]),
+			(0.50, [0, 0.2, 0.4, 0.6, 0.8, 1, 0, 0, 0, 0, 0]),
+			(0.60, [0, 0.166666667, 0.333333333, 0.5, 0.666666667, 0.833333333, 1, 0, 0, 0, 0]),
+			(0.70, [0, 0.142857143, 0.285714286, 0.428571429, 0.571428571, 0.714285714, 0.857142857, 1, 0, 0, 0]),
+			(0.80, [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1, 0, 0]),
+			(0.90, [0, 0.111111111, 0.222222222, 0.333333333, 0.444444444, 0.555555556, 0.666666667, 0.777777778, 0.888888889, 1, 0]),
+			(1.00, [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]),
+		]
+
+		for peak, exepcted_output_list in test_data:
+			peaked_utility = create_peaked_utility(peak, True)
+			self.check_expected_output_for_range_0_1('peaked_cutoff_'+str(peak), peaked_utility, exepcted_output_list)
+
+
+
+	def test_spiked_utility(self):
+		test_data = [
+			(0.00, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+			(0.50, [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]),
+			(1.00, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+		]
+
+		for spike, exepcted_output_list in test_data:
+			spiked_utility = create_spiked_utility(spike)
+			self.check_expected_output_for_range_0_1('spiked_cutoff_'+str(spike), spiked_utility, exepcted_output_list)
+
+
+		# test close values
+		spiked_utility = create_spiked_utility(0.5)
+
+		small_values = [9.99999e-8, 1e-10, 1e-11]
+
+		for val in small_values:
+			with self.subTest(name='spiked_cutoff_close +', val=val):
+				output = spiked_utility(0.5 + val)
+				self.assertAlmostEqual(output, 1.0)
+			with self.subTest(name='spiked_cutoff_close -', val=val):
+				output = spiked_utility(0.5 - val)
+				self.assertAlmostEqual(output, 1.0)
+
+
+
+	def check_expected_output_for_range_0_1(self, name, utility_function, exepcted_output_list):
+		for unlike_neighbors, exepcted_output in zip(linspace(0.0, 1.0, 11), exepcted_output_list):
+				with self.subTest(name=name, input=unlike_neighbors):
+					output = utility_function(round(unlike_neighbors, 4))
+					self.assertAlmostEqual(output, exepcted_output)
+
+
+	def test_value_error_outside_0_1(self):
+		functions = {
+			'flat': create_flat_utility(0.5),
+			'peaked': create_peaked_utility(0.5),
+			'peaked_cutoff': create_peaked_utility(0.5, True),
+			'spiked': create_spiked_utility(0.5),
+			'create_flat': create_flat_utility,
+			'create_peaked': create_peaked_utility,
+			'create_spiked': create_spiked_utility,
+		}
+
+		error_inputs = [-10, -1, -0.1, -0.00001, 1.00001, 1.1, 2, 10]
+
+		for name, function in functions.items():
+			for error_input in error_inputs:
+				with self.subTest(name=name, input=error_input):
+					with self.assertRaises(ValueError):
+						function(error_input)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
---
+++ schelling/utility_functions.py
@@ -0,0 +1,110 @@
+from functools import wraps
+from math import isclose
+
+def range_check_0_1(function):
+	"""Raises value error, if range of first argument is not in [0, 1]"""
+
+	@wraps(function)
+	def checked_function(*args, **kwargs):
+
+		value=args[0]
+		if value < 0.0 or value > 1.0:
+			raise ValueError("Argument must be between 0.0 and 1.0")
+
+		return function(*args, **kwargs)
+
+	return checked_function
+
+
+@range_check_0_1
+def create_flat_utility(threshold):
+	"""Creates a flat utility function with the specified threshold of unlike neighbors
+	Flat utility: u(t) = 1, if t <= threshold; 0, if t > threshold
+	
+	Args:
+	    threshold (float): threshold - in range [0.0, 1.0]
+	
+	Returns:
+	    function: utility function
+	"""
+
+	@range_check_0_1
+	def flat_utility(unlike_neighbor_fraction):
+
+		if unlike_neighbor_fraction <= threshold:
+			return 1.0
+		else:
+			return 0.0
+	
+	return flat_utility
+
+
+@range_check_0_1
+def create_peaked_utility(peak, cutoff=False):
+	"""Creates peaked utility function with 
+	peak value at specified fraction of unlike neighbors and optional cutoff
+	Peaked utility without cutoff:
+		u(t) = (1 / peak) * t, if t < peak; 1 - (1/(1-peak))*(t - peak), if t >= peak
+
+	Peaked utility with cutoff:
+		u(t) = (1 / peak) * t, if t <= peak; 0, if t >= peak
+
+	Args:
+	    peak (float): Utility function peaks at this argument - in range [0, 1]
+	    cutoff (bool, optional): Sets cutoff behavior
+	
+	Returns:
+	    function: utility function
+	"""
+
+	try:
+		left_gradient = 1 / peak
+	except ZeroDivisionError:
+		left_gradient = 1.0
+	
+	try:
+		right_gradient = (-1 / (1 - peak))
+	except ZeroDivisionError:
+		right_gradient = 1.0
+
+	@range_check_0_1
+	def peaked_utility(unlike_neighbor_fraction):
+		if unlike_neighbor_fraction < peak:
+			return left_gradient * unlike_neighbor_fraction
+		else:
+			return (right_gradient * (unlike_neighbor_fraction - peak)) + 1
+
+	@range_check_0_1
+	def peaked_utility_cutoff(unlike_neighbor_fraction):
+		if unlike_neighbor_fraction <= peak:
+			return left_gradient * unlike_neighbor_fraction
+		else:
+			return 0.0
+
+	if cutoff:
+		return peaked_utility_cutoff
+	else:
+		return peaked_utility
+
+
+@range_check_0_1
+def create_spiked_utility(spike):
+	"""Create spiked utility function with spike at specified fraction of unlike neighbors
+	u(t) = 1, if t = spike; 0, otherwise
+	
+	Args:
+	    spike (float): Spike at this argument - in range [0, 1]
+	
+	Returns:
+	    function: utility function
+	"""
+	@range_check_0_1
+	def spiked_utility(unlike_neighbor_fraction):
+		if isclose(unlike_neighbor_fraction, spike, abs_tol=1e-7):
+			return 1.0
+		else:
+			return 0.0
+
+	return spiked_utility
+
+
