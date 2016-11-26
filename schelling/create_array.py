e1369e5dc3382c7b347baf041458e5b243fe5c89
Stas Horawa <stashorawa@gmail.com>
2016-11-26 21:56:58 +0100

    Adding random array creation

---
+++ schelling/create_array.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def create_array(size, agent_type_fractions):
+	"""Creates a square 2d numpy array representing a 2d grid randomly populated by agents.
+	Nodes with value 0 are vacant
+	Nodes with value n are occupied by agent of type n (n - integer value)
+
+	Example:
+	create_array(50, (0.2, 0.5, 0.3)) will create a grid of 50*50 = 2500 spots.
+	0.2 * 2500 = 500 will be vacant
+	0.5 * 2500 = 1250 will be occupied by agents of type 1.
+	0.4 * 2500 = 750 will be occupied by agents of type 2.
+	
+	Args:
+	    size (int): size of grid side
+	    agent_type_fractions (tuple of float): fraction of each agent type. 
+	    	First element represents fraction of vacancies.
+	    	Subsequent elements represent fractions of agents of each type
+	
+	Returns:
+	    ndarray: array representing 2d grid randomly populated by agents
+	"""
+	if float(sum(agent_type_fractions)) != 1.0:
+		raise ValueError("agent type fractions must sum up to 1")
+
+	n_cells = size * size
+	
+	agent_type_counts = [int(n_cells * agent_type_fraction) 
+	  for agent_type_fraction in agent_type_fractions]
+
+	array_data = []
+	for agent_type_index, agent_type_count in enumerate(agent_type_counts):
+		array_data += ([agent_type_index] * agent_type_count)
+
+	# The number of cells could be one less than 
+	# size*size due to rounding down floats.
+	# In that case, append an additional vacant slot.
+	if len(array_data) != n_cells:
+		array_data += [0]
+
+	array = np.array(array_data)
+
+	np.random.shuffle(array)
+	array = array.reshape((size, size))
+	return array
---
+++ schelling/test/test_create_array.py
@@ -0,0 +1,58 @@
+from ..create_array import create_array
+from decimal import Decimal
+import unittest
+import numpy as np
+
+class CreateArrayTest(unittest.TestCase):
+
+	def test_array_properties(self):
+		arr = create_array(50, (0.2, 0.4, 0.4))
+		self.assertEqual(arr.ndim, 2)
+		self.assertEqual(arr.shape, (50, 50))
+		self.assertEqual(arr.size, 2500)
+		self.assertEqual(arr.dtype, 'int64')
+
+
+	def test_agent_type_counts(self):
+		# arr_size (int), agent_proportions (tuple), has rounding error (bool)
+		parameters_list = [
+			(50, (0.2, 0.4, 0.4), False),
+			(2, (0.1, 0.4, 0.5), True),
+			(100, (1/3,1/3,1/3), True),
+			(1000, (1/3,1/3,1/3), True),
+			(100, (0.25, 0.25, 0.25, 0.25), False),
+			(1000, tuple([Decimal(0.01)]*100), False),
+
+		]
+
+		for arr_size, agent_proportions, has_rounding_error in parameters_list:
+			arr = create_array(arr_size, agent_proportions)
+
+			for agent_index, agent_proportion in enumerate(agent_proportions):
+				with self.subTest(arr_size=arr_size, agent_proportions=agent_proportions, agent_index=agent_index):			
+					expected_count = int((arr_size**2) * agent_proportion)
+					if agent_index == 0 and has_rounding_error: 
+						expected_count += 1
+
+					arr_where_agent = arr == agent_index
+					count = arr_where_agent.sum()
+					
+					self.assertEqual(expected_count, count)				
+
+
+	def test_agent_fractions_input_should_equal_one_error(self):
+		parameters_list = [
+			(50, (0.2, 0.2, 0.2)),
+			(50, (0.2, 0.6, 0.6)),
+			(50, ()),
+			(50, (100, 100, 100)),
+			(50, (0.33, 0.33, 0.33)),
+		]
+
+		for parameters in parameters_list:
+			with self.subTest(parameters=parameters):
+				with self.assertRaises(ValueError):
+					create_array(*parameters)					
+
+if __name__ == '__main__':
+	unittest.main()
