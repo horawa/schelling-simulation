309c7fa48f70f0d234d2864ab4d816ec700fc7c0
Stas Horawa <stashorawa@gmail.com>
2016-12-13 02:17:26 +0100

    Added exponential and normal distribution

--- schelling/agent_distributions.py
+++ schelling/agent_distributions.py
@@ -1,3 +1,7 @@
+import scipy.integrate as integrate
+import numpy as np
+from math import exp, sqrt, pi
+
 def get_uniform_distribution(agent_count):
 	if agent_count < 1 or agent_count > 7:
 		raise ValueError(
@@ -7,6 +11,66 @@ def get_uniform_distribution(agent_count):
 	return tuple(uniform_distribution)
 
 
+def get_exponential_distribution(lambda_=1.0):
+	if lambda_ <= 0:
+		raise ValueError("lambda_ must be > 0")
+
+	def exponential_dist_func(x):
+		return lambda_*exp(-lambda_*x)
+
+	max_agent_count = 7
+
+	agent_fractions = []
+
+	for agent in range(max_agent_count - 1):
+		fraction = integrate.quad(exponential_dist_func, agent, agent + 1)[0]
+		agent_fractions.append(fraction)
+
+	agent = max_agent_count - 1
+	last_fraction = integrate.quad(exponential_dist_func, agent, np.inf)[0]
+	agent_fractions.append(last_fraction)
+
+	return agent_fractions
+
+
+def get_normal_distribution(std_dev):
+	if std_dev <= 0:
+		raise ValueError("std_dev must be > 0")
+
+	mean = 4
+	def normal_dist_func(x):
+		n = (1/sqrt(2*pi*(std_dev**2))) * exp(-(((x-mean)**2)/(2*(std_dev**2))))
+		return n
+
+	# (1/sqrt(2*pi*(1^2))) * exp(-(((x-4)^2)/(2*(1^2))))
+
+	max_agent_count = 7
+
+	agent_fractions = []
+
+	# -inf, 1.5
+ 	# 1.5, 2.5
+	# 2.5, 3.5
+	# 3.5, 4.5: mean +/- 0.5 
+	# 4.5, 5.5
+	# 5.5, 6.5
+	# 6.5, inf
+
+	agent = 0
+	first_fraction = integrate.quad(normal_dist_func, -np.inf, 1.5)[0]
+	agent_fractions.append(first_fraction)
+
+	for agent in range(1, max_agent_count - 1):
+		fraction = integrate.quad(normal_dist_func, agent + 0.5, agent + 1.5)[0]
+		agent_fractions.append(fraction)
+
+	agent = max_agent_count - 1
+	last_fraction = integrate.quad(normal_dist_func, agent + 0.5, np.inf)[0]
+	agent_fractions.append(last_fraction)
+
+	return agent_fractions
+
+
 def get_distribution_including_vacancies(vacancy_proportion, distribution):
 	agent_proportion = 1.0 - vacancy_proportion
 	agent_type_proporitons = \
@@ -14,3 +78,4 @@ def get_distribution_including_vacancies(vacancy_proportion, distribution):
 	distribution_including_vacancies = \
 		[vacancy_proportion] + agent_type_proporitons
 	return tuple(distribution_including_vacancies)
+
--- schelling/test/test_agent_distributions.py
+++ schelling/test/test_agent_distributions.py
@@ -2,6 +2,8 @@ import unittest
 from math import isclose
 
 from ..agent_distributions import (get_uniform_distribution, 
+	get_exponential_distribution,
+	get_normal_distribution,
 	get_distribution_including_vacancies)
 
 from ..simulation_settings import SimulationSettings
@@ -27,6 +29,67 @@ class DistributionsTestCase(unittest.TestCase):
 			with self.subTest():
 				with self.assertRaises(ValueError):
 					get_uniform_distribution(value)
+
+
+	def test_get_exponential_distribution(self):
+		"""
+		wolfram:
+
+		integrate exp(-x) on (0, 1);
+		integrate exp(-x) on (1, 2);
+		integrate exp(-x) on (2, 3);
+		integrate exp(-x) on (3, 4);
+		integrate exp(-x) on (4, 5);
+		integrate exp(-x) on (5, 6);
+		integrate exp(-x) on (6, infinity);
+
+		etc. 
+		"""
+
+		parameters = [
+			(1, (0.63212, 0.23254, 0.085548, 0.031471, 0.011578, 
+				0.0042592, 0.0024788)),
+			(2, (0.86466, 0.11702, 0.015837, 0.0021433, 0.00029006, 
+				0.000039256, 6.1442e-6))
+		]
+
+		for lambda_, expected_output in parameters:
+			output = get_exponential_distribution(lambda_)	
+			for out_val, ex_val in zip(output, expected_output):
+				with self.subTest(lambda_=lambda_, out=out_val, ex=ex_val):
+					vals_close = isclose(out_val, ex_val, rel_tol=1e-04)
+					self.assertTrue(vals_close)
+
+
+	def test_get_normal_distribution(self):
+		"""
+		dev1
+		0.00621, 0.06681, 0.30854, 0.69146, 0.93319, 0.93319, 1, 	
+		(0.006209665, 0.060597536, 0.241730337, 0.241730337, 
+			0.241730337, 0.060597536, 0.006209665)
+
+		dev0.5
+		0.00002, 0.00135, 0.15866, 0.84134, 0.99865, 0.99998, 1
+		(2.866515719E-7, 0.001349611, 0.157305356, 0.682689492, 
+			0.157305356, 0.001349611, 2.866515719E-7)
+		"""
+
+		parameters = [
+			(1, (0.006209665, 0.060597536, 0.241730337, 0.382924923, 
+				0.241730337, 0.060597536, 0.006209665)),
+			(0.5, (2.866515719E-7, 0.001349611, 0.157305356, 0.682689492, 
+				0.157305356, 0.001349611, 2.866515719E-7))
+		]
+
+
+		for sd, expected_output in parameters:
+			output = get_normal_distribution(sd)
+			for out_val, ex_val in zip(output, expected_output):
+				with self.subTest(sd=sd, out=out_val, ex=ex_val):
+					vals_close = isclose(out_val, ex_val, rel_tol=1e-04)
+					self.assertTrue(vals_close)				
+
+
 
 	def test_get_distribution_including_vacancies(self):
 		parameters = [
