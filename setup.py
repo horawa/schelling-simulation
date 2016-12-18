9ec001cf5cb0d50a43f6f2889b4e535ccde28d31
Stas Horawa <stashorawa@gmail.com>
2016-12-18 18:11:02 +0100

    Removed install_requires. Using requirements.txt instead

--- setup.py
+++ setup.py
@@ -14,13 +14,6 @@ setup(
 	author='Stas Horawa',
 	author_email='stashorawa@gmail.com',
 	packages=['schelling'],
-	install_requires=[
-		"numpy>=1.12.0b1,<2.0.0",
-		"scipy>=0.18.1,<1.0.0",
-		"matplotlib>=1.5.3,<2.0.0",
-		"scikit-image>=0.12.3,<1.0.0",
-		"click>=6.6,<7.0",
-	],
 	include_package_data=True,
 	zip_safe=False,
     scripts=['bin/run-simulation'],
