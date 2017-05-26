589e7fdb6ed67bdb1ecce1f6ee14b19bfdda0005
Stas Horawa <stashorawa@gmail.com>
2017-05-26 13:01:39 +0200

    Added utility script to get results

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
 	include_package_data=True,
 	zip_safe=False,
 	scripts=['bin/run-simulation', 'bin/run-v-T', 
-		'bin/verify_simulations_halted'],
+		'bin/verify_simulations_halted', 'bin/get_results'],
 	entry_points={
 		'console_scripts': ['schelling-cli=schelling.cli:simulation'],
 	},
